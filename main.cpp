a094c60ab058484aedb64b1191eab5e543bcff95
Valiant49 <vichandreicortez49@gmail.com>
2025-12-05 15:38:40 +0800

    made calculation looping

--- main.cpp
+++ main.cpp
@@ -2,13 +2,60 @@
 
 using namespace std;
 
-int main() {
-    float operand1, operand2 = 0;
+struct passedValues
+{
     int option;
-    float sum, diff, prod, quot;
+    float operand1;
+    float operand2;
+
+};
+
+void showHomeMenu();
+passedValues getInput();
+void operationCalculations(const passedValues& input, float total);
+char showMenu();
+
+char continueReply;
+float grandTotal;
+
+int main() {
+    float total;    
+    bool loop = true;
+    while (loop == true)
+    {
+        passedValues input = getInput();
+        operationCalculations(input,total);
+        continueReply = showMenu();
+        if (continueReply == 'n')
+            {
+                cout << "Bye!" << endl;
+                break;
+            }
+    }
+}
+
+
+
+
+void showHomeMenu() {
+    cout << "Simple Calculator" << endl;
+    cout << "Options:" << endl;
+    cout << "1. Open Calculator \n2. Enter TUI Mode \n3. Exit" << endl;
+}
+
+char showMenu() {
+    char reply;
+    cout << "Continue? [y/N]";
+    cin >> reply;
+    
+    return tolower(reply);
+}
+
+passedValues getInput() {
+    passedValues input;
 
     cout << "Enter operand1: ";
-    cin >> operand1;
+    cin >> input.operand1;
     
     cout << "\n Select operation:" << endl;
     cout << "1. Add: +" << endl;
@@ -16,32 +63,35 @@ int main() {
     cout << "3. Multiply: *" << endl;
     cout << "4. Divide: /" << endl;
     cout << "Your option: ";
-    cin >> option; 
+    cin >> input.option; 
 
     cout << "\n Enter operand2: ";
-    cin >> operand2;    
+    cin >> input.operand2;
 
-    switch (option) {
+    return input;
+}
+
+void operationCalculations(const passedValues& input, float total) 
+{
+    switch (input.option) {
     case 1:
-        sum = operand1 + operand2;
-        cout << "The result is "  << sum << endl;
+        total = input.operand1 + input.operand2;
+        cout << "The result is "  << total << endl;
         break;
     case 2:
-        diff = operand1 - operand2;
-        cout << "The result is " << diff << endl;
+        total = input.operand1 - input.operand2;
+        cout << "The result is " << total << endl;
         break;
     case 3:
-        prod = operand1 * operand2;
-        cout << "The result is " << prod << endl;
+        total = input.operand1 * input.operand2;
+        cout << "The result is " << total << endl;
         break;
     case 4:
-        quot = operand1 / operand2;
-        cout << "The result is " << quot << endl;
+        total = input.operand1 / input.operand2;
+        cout << "The result is " << total << endl;
         break;
     default:
         cout << "Invalid option. Choose a valid option.";
         break;
     }
-
-    return 0;
 }
